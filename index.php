40e19b2efed363f7b420500c9b902150970e2a84
Нелли Федорова <90643474+Nelli88Fedorova@users.noreply.github.com>
2022-05-07 13:33:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         </label><br />
 
         <input name="buttlog" type="submit" value="Enter" />
-        <input name="registration" type="submit" value="Registration" />
+        <input name="buttlog" type="submit" value="Registration" />
         <input name="buttlog" type="submit" value="Admin" />
       </form>
 
@@ -95,21 +95,19 @@ else {
     }
   if ($button == 2) //Регистрация
   {
-    // if (isset($_COOKIE['all_OK'])) {
-    //   unset($_SESSION['uid']);
-    //   unset($_SESSION['login']);
-    //   session_destroy();
-    //   setcookie('all_OK', '', time() - 1000);
-    //   setcookie('login', '', time() - 1000);
-    //   setcookie('pass', '', time() - 1000);
-    //   header('Location: user.php');
-    //   exit();
-    // } else {
-    //   header('Location: user.php');
-    //   exit();
-    // }
-    header('Location: user.php');
+    if (isset($_COOKIE['all_OK'])) {
+      unset($_SESSION['uid']);
+      unset($_SESSION['login']);
+      session_destroy();
+      setcookie('all_OK', '', time() - 1000);
+      setcookie('login', '', time() - 1000);
+      setcookie('pass', '', time() - 1000);
+      header('Location: user.php');
       exit();
+    } else {
+      header('Location: user.php');
+      exit();
+    }
   } else if ($button == 3) {//Админ
     if (isset($_COOKIE['all_OK'])) {
       unset($_SESSION['uid']);
